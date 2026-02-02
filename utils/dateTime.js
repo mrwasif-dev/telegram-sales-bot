4b885151fbdd0836536a35756545e89e06a34714
mrwasif-dev <happyprincetemp@gmail.com>
2026-02-02 08:41:17 +0500

    Create dateTime.js

---
+++ utils/dateTime.js
@@ -0,0 +1,25 @@
+function getCurrentDateTime() {
+    const d = new Date();
+    const utc = d.getTime() + d.getTimezoneOffset() * 60000;
+    const pakistanTime = new Date(utc + 5 * 60 * 60 * 1000);
+
+    const date = `${String(pakistanTime.getDate()).padStart(2,'0')}-${String(pakistanTime.getMonth()+1).padStart(2,'0')}-${pakistanTime.getFullYear()}`;
+    const time = `${String(pakistanTime.getHours()).padStart(2,'0')}:${String(pakistanTime.getMinutes()).padStart(2,'0')}:${String(pakistanTime.getSeconds()).padStart(2,'0')}`;
+
+    return { date, time };
+}
+
+function formatDate(dateString) {
+    const [day, month, year] = dateString.split('-');
+    return `${year}-${month}-${day}`;
+}
+
+function getTimestamp() {
+    return Date.now();
+}
+
+module.exports = {
+    getCurrentDateTime,
+    formatDate,
+    getTimestamp
+};
